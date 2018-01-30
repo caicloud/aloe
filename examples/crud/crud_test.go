ac14c97d9ff7c4d3704f0899b563eea0b25c4f8c
zhengjiajin <zhengjiajin@caicloud.io>
2018-01-30 20:17:15 +0800

    fix(example): add stopCh ensure http server started

--- examples/crud/crud_test.go
+++ examples/crud/crud_test.go
@@ -2,8 +2,17 @@ package crud
 
 import (
 	"testing"
+
+	"github.com/caicloud/aloe/examples/crud/server"
+	"github.com/onsi/ginkgo"
 )
 
 func TestCRUD(t *testing.T) {
 	RunTEST(t)
 }
+
+var _ = ginkgo.BeforeSuite(func() {
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	go server.RunServer(stopCh)
+})
